bd6407fde54ca58c58f6a890869ab322fbfcf34d
- <78488184+exragexe@users.noreply.github.com>
2023-06-10 21:45:25 +0300

    lab 2 done

--- labs/lib/integrate/application.dart
+++ labs/lib/integrate/application.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:labs/screens/home.dart';
+import 'package:provider/provider.dart';
 
 class Application extends StatelessWidget {
   const Application({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-      '/': (context) => const Home(),
-      },
-      debugShowCheckedModeBanner: false,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+      
     );
   }
 }
